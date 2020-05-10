2f848c1edfcd0bdb356e6cea13963e0f5296ccdf
herrhundt <herr_hundt@online.de>
2020-05-10 14:59:21 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+public class Controller
+{
+    public void pressButton(ActionEvent evt)
+    {
+        System.out.println("Test");
+    }
+
+}
