0f1003d64f7857024c8fd034b4a9541e28b12f38
Picman2000 <65342372+Picman2000@users.noreply.github.com>
2020-05-26 22:45:48 +0200

    überraschung

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,14 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.awt.*;
 import java.net.URI;
+import java.util.Optional;
 
 
 public class Controller {
@@ -29,29 +31,51 @@ public class Controller {
     }
     private void showAlertWithHeaderText1() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("You got jebaited");
-        Image image = new Image("https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg");
+        alert.setTitle("Was ist denn hier los?");
+        Image image = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Pfeil_rechts_unten.svg/768px-Pfeil_rechts_unten.svg.png");
         ImageView imageView = new ImageView(image);
         alert.setGraphic(imageView);
+        alert.setHeaderText("Will mir der Pfeil etwas sagen?");
+        alert.setContentText("Was hat das zu bedeuten und warum ist der Pfeil so groß? Erfahre es dort drüben -->");
 
-        Hyperlink link = new Hyperlink();
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent()) {
+            try{
+                Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ")) ;
+            }
+
+            catch(Exception e){
+                alert.close();
+            }
+
+
+
+
+        }
+
+        alert.showAndWait();
+    }
+
+
+
+        /* Hyperlink link = new Hyperlink();
         link.setText("http://example.com");
         link.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
                 //hyperlink.setOnAction(Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ")));
             }
-        });
+        }); */
 
         //Hyperlink hyperlink = new Hyperlink("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
-        alert.getButtonTypes();
-
-        alert.setHeaderText("http://example.com");
 
 
+        //alert.setHeaderText("http://example.com");
 
-        alert.showAndWait();
+
+
+
     }
 
-}
+
